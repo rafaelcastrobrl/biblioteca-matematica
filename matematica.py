386062bb917495347fb3da227e311a8924b73726
Rafael Guimarães de Castro <rafael@rafaelcastro.com>
2019-10-19 14:05:38 -0300

    Atualizando primos

--- matematica.py
+++ matematica.py
@@ -20,6 +20,6 @@ def primo(num):
             div = div + 1
         valor = valor + 1
     if div == 2:
-        print('É primo!')
+        return True
     else:
-        print('Não é primo!')
+        return False
--- user.py
+++ user.py
@@ -3,4 +3,7 @@ import matematica as m
 num = int(input('Informe um número para apresentar o seu fatorial: '))
 resposta = m.fatorial(num)
 print(resposta)
-m.primo(num)
+if m.primo(num):
+    print('É primo!!')
+else:
+    print('Não é primo!!')
